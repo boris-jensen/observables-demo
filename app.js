1a201dca22b76a885c68b99b03329a0a4257cfc3
Boris Søborg Jensen <boris.jensen@d4tec.com>
2018-12-01 16:17:55 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+let doWait = false
+
+app.get('/ping', (req, res) => res.send('pong'))
+
+app.get('/typeahead', (req, res) => {
+    const response = [1, 2, 3].map(i => `${req.query.query}${i}`)
+    const timeout = doWait ? 2000 : 0
+    doWait = !doWait;
+
+    setTimeout(() => res.send(response), timeout)
+})
+
+app.use(express.static('public'))
+
+app.listen(port, () => console.log(`Rx app listening on port ${port}!`))
