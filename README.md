7401ac47aa52ec556ebce70887a9075bdce0ff17
Boris Søborg Jensen <boris.jensen@d4tec.com>
2018-12-02 20:21:47 +0100

    Add basic java console app

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 https://rxjs-dev.firebaseapp.com/api
 http://reactivex.io/documentation/operators.html
+
+cd rx-java
+mvn clean package
+java -jar target/rx-java-1.0-SNAPSHOT.jar
---
+++ rx-java/src/test/java/my/group/AppTest.java
@@ -0,0 +1,15 @@
+package my.group;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+    @Test
+    public void testHex() {
+        junit.framework.Assert.assertEquals(true, true);
+    }
+}
