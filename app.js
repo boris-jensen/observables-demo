b8295f4a7f90ff445025940b4542c1c6792c6c2b
Boris Søborg Jensen <boris.jensen@d4tec.com>
2018-12-07 03:12:13 +0100

    Cleanup

--- app.js
+++ app.js
@@ -4,8 +4,7 @@ const port = 3000
 
 let doWait = false
 
-app.get('/ping', (req, res) => res.send('pong'))
-
+// send "a", get 3 'completions': ["a1", "a2", "a3"], with varied response times
 app.get('/typeahead', (req, res) => {
     const response = [1, 2, 3].map(i => `${req.query.prefix}${i}`)
     const timeout = doWait ? 2000 : 0
--- warmups/warmup4.js
+++ warmups/warmup4.js
@@ -1,12 +1,12 @@
 const rxjs = require('rxjs')
 
-const {map} = require('rxjs/operators')
-
 const observer = {
     next: (i) => console.log('new value: ' + i),
     error: (err) => console.log('error: ' + err),
     complete: () => console.log('completed')
 }
+
+const {map} = require('rxjs/operators')
 
 // Syntax error for rxjs
 // rxjs.of(1,2,3).map( .... 
