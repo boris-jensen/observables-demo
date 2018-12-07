330bf5718b456fc95ba08f35aa25bcb543e56fab
Boris Søborg Jensen <boris.jensen@d4tec.com>
2018-12-07 03:20:41 +0100

    Add tap to warmup example

--- warmups/warmup5.js
+++ warmups/warmup5.js
@@ -6,7 +6,7 @@ const observer = {
     complete: () => console.log('completed')
 }
 
-const {map, mergeAll, filter, mapTo, take} = require('rxjs/operators')
+const {map, mergeAll, filter, mapTo, take, tap} = require('rxjs/operators')
 
 const makeInnerObservable = (i) => rxjs
     .interval(300)
@@ -15,8 +15,9 @@ const makeInnerObservable = (i) => rxjs
         take(8 - i))
 
 const outerObservable = rxjs
-    .interval(200)
+    .interval(200) // [0, 1, 2, 3, 4, 5, 6, 7,.....]
     .pipe(
+        tap(i => console.log('tapped: ' + i)), // For debugging. Pass through values, but allow side effects
         filter(i => i % 2 === 0), // [0, 2, 4, 6, 8, ...]
         take(4), // [0, 2, 4, 6]
         map(makeInnerObservable), // [[0,0,0,0,0,0,0,0], [2,2,2,2,2,2], [4,4,4,4], [6,6]]
